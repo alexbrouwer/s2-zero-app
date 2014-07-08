13b77b87d2f4e0a1f240d0242002af106452f43c
alexbrouwer <brouwer.alexander@gmail.com>
2014-07-08 14:05:50 +0200

    Added UTC for UserGroupType

---
+++ src/Zero/UserBundle/Tests/Form/User/GroupTypeTest.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Zero\UserBundle\Tests\Form\User;
+
+
+use Symfony\Component\Form\Test\TypeTestCase;
+use Zero\UserBundle\Entity\User\Group;
+use Zero\UserBundle\Form\User\GroupType;
+
+class GroupTypeTest extends TypeTestCase
+{
+    public function testSubmitValidData()
+    {
+        $formData = array(
+            'name'    => 'name',
+            'roles'       => array('testRole')
+        );
+
+        $object = new Group();
+        $object->setName($formData['name']);
+        $object->setRoles($formData['roles']);
+
+        $type = new GroupType();
+        $form = $this->factory->create($type);
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($object, $form->getData());
+
+        $view     = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
+ 
