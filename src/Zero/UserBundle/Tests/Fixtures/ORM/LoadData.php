ef7761f23d25dbed1a343c4256b8be1403549ec8
alexbrouwer <brouwer.alexander@gmail.com>
2014-07-09 13:00:37 +0200

    Removed numbers from variable

--- src/Zero/UserBundle/Tests/Fixtures/ORM/LoadData.php
+++ src/Zero/UserBundle/Tests/Fixtures/ORM/LoadData.php
@@ -12,30 +12,34 @@ class LoadData implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $group1 = new User\Group();
-        $group1->setName('group1');
-        $group1->setRoles(array('ROLE1', 'ROLE2'));
-        $manager->persist($group1);
-
-        $group2 = new User\Group();
-        $group2->setName('group2');
-        $group2->setRoles(array('ROLE3', 'ROLE2'));
-        $manager->persist($group2);
+        $group = $this->createGroup('group1', array('ROLE1', 'ROLE2'));
+        $manager->persist($group);
 
         $user = new User();
         $user->setUsername('username1');
         $user->setEmail('username1@example.com');
         $user->setDisplayName('User 1');
-        $user->addGroup($group1);
+        $user->addGroup($group);
         $manager->persist($user);
+
+        $group = $this->createGroup('group2', array('ROLE2', 'ROLE3'));
+        $manager->persist($group);
 
         $user = new User();
         $user->setUsername('username2');
         $user->setEmail('username2@example.com');
         $user->setDisplayName('User 2');
-        $user->addGroup($group2);
+        $user->addGroup($group);
         $manager->persist($user);
 
         $manager->flush();
+    }
+
+    private function createGroup($name, array $roles) {
+        $group = new User\Group();
+        $group->setName($name);
+        $group->setRoles($roles);
+
+        return $group;
     }
 } 
